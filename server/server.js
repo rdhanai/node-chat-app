35be6092e449b96621150532a93fdde1be95b844
Rajpal Singh <rdhanai@innominds.com>
2018-10-14 17:54:41 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+
+const port = process.env.port || 3000;
+
+const app = express();
+app.use(express.static(publicPath));
+
+console.log('hi', publicPath);
+app.listen(port, () => {
+    console.log('app is up and running on port' +  port);
+});
+
