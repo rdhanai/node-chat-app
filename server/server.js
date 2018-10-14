1fefde5e90f682f04254b32aca959d77490ee106
Rajpal Singh <rdhanai@innominds.com>
2018-10-14 22:19:41 +0530

    connect and disconnect events added

--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
-app.use(express.static(publicPath));
+const server = http.createServer(app);
 
+var io = socketIO(server);
+
+io.on('connection', (socket)=>{
+    console.log('new user connected..');
+    socket.on('disconnect', ()=>{
+        console.log('disconnted from client');
+    });
+});
+
+
+app.use(express.static(publicPath));
 console.log('hi', publicPath);
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('app is up and running on port' +  port);
 });
 
