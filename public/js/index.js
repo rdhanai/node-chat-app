ac31c1a45d45f5327185fbb87e5a2e2946e6de13
Rajpal Singh <rdhanai@innominds.com>
2018-10-16 20:09:27 +0530

    emit and broadcast..

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+var socket = io();
+socket.on('connect', function(){
+    console.log('connected to server');
+});
+socket.on('disconnect', function() {
+    console.log('disconnected from server');
+});
+
+socket.on('newMessage', function(data) {
+    console.log('new message received from server..', data.text);
+});
--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
-
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
 
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
-const app = express();
-const server = http.createServer(app);
 
+const app = express();
+app.use(express.static(publicPath));
+const server = http.createServer(app);
 var io = socketIO(server);
 
 io.on('connection', (socket)=>{
@@ -17,11 +17,18 @@ io.on('connection', (socket)=>{
     socket.on('disconnect', ()=>{
         console.log('disconnted from client');
     });
+
+    socket.on('createMessage', (message)=>{
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
+    });
+
 });
 
-
-app.use(express.static(publicPath));
-console.log('hi', publicPath);
 server.listen(port, () => {
     console.log('app is up and running on port' +  port);
 });
