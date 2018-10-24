68809917e1f19faec8260786257738ad02dd8505
Rajpal Singh <rdhanai@innominds.com>
2018-10-24 20:06:45 +0530

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,18 @@
 var socket = io();
+
+function scrollToBottm(){
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}   
 socket.on('connect', function(){
     console.log('connected to server');
 });
@@ -16,6 +30,7 @@ socket.on('newMessage', function(data) {
     });
     var messages = jQuery('#messages');
     messages.append(html);
+    scrollToBottm();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -28,6 +43,7 @@ socket.on('newLocationMessage', function (message) {
     });
     var messages = jQuery('#messages');
     messages.append(html);
+    scrollToBottm();
 
     // var formattedTime = moment(message.createdAt).format('h:mm a');
     // var li = jQuery('<li></li>');
